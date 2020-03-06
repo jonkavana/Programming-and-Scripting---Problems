48192688bdf2c96b4f5b3899dca17cdfe259d80b
jonkavana <G00387868@gmit.ie>
2020-03-06 11:31:29 +0000

    added 2 new files

---
+++ divides.py
@@ -0,0 +1,12 @@
+# John Kavanagh
+# check to see if one number divides another
+
+p = 8
+m = 2
+
+if p % m == 0:
+    print (p, "divided by", m, "leaves no remainder")
+else: 
+    print (p, "divided by", m, "leaves a remainder")
+
+print("I'll be run no matter what.")
