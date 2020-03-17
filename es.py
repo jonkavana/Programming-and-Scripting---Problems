d05512bf619ee43df6f91b6bd2cee6eeb6f63f65
jonkavana <G00387868@gmit.ie>
2020-03-17 14:33:36 +0000

    new files from exercises

---
+++ es.py
@@ -0,0 +1,9 @@
+# John Kavanagh
+# moby-dick.txt
+# Write a program that reads in a text file and outputs the number of e's it contains.
+# The program should take the filename from an argument on the command line.
+
+
+with open('moby-dick.txt', 'r') as f 
+    print(f.readline(moby-dick.txt))
+    
---
+++ readafileexplanation.py
@@ -0,0 +1,5 @@
+# John Kavanagh
+
+f = open('.gitignore', 'r')
+f.close()
+
