2bdb0cc93de8abfbeb35e976b51200de6f478316
jonkavana <G00387868@gmit.ie>
2020-04-05 22:29:13 +0100

    changes to the exercise file as well as testing some objects

--- testing datetime.py
+++ testing datetime.py
@@ -1,14 +1,13 @@
-# John Kavanagh
-# Create a program that will identify if a day is a weekday or weekend
+# need to work out how the datetime obkect works
 
 import datetime
-import calendar
 
 L = {0:'Monday', 1:'Tuesday', 2:'Wednesday', 3:'Thursday', 4:'Friday', 5:'Saturday', 6:'Sunday'}
 dayno=datetime.datetime.today().weekday()
+
 print("Today is", L[dayno])
 
 if dayno < 4:
     print("Hauld your horeses")
 else:
-    print("You've made it ,now reward yourself.")
+    print("induldge")
