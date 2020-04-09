a5a12b140f639afb3ef2a2e07eacd422ceea8216
jonkavana <G00387868@gmit.ie>
2020-04-09 17:29:05 +0100

    minor changes to layout

--- squareroot.py
+++ squareroot.py
@@ -1,12 +1,9 @@
 # John Kavanagh
-# need to create a function called sqrt, that finds the square root of any positive number
-
-#def sqrt(x):
-#    return x ** (1/2)
+# need to create a function that finds the square root of any positive number
 
 x = (float(input("Type in any positive number: ")))
 
 def sqrt(x):
-    return x ** (1/2)
+    return round(x ** (1/2), 1)
 
 print("The square root of", x, "is approximately", sqrt(x))
