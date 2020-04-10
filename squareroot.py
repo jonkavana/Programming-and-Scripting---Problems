71d6e7f817294e290491c93ef310f19dd71bd63c
jonkavana <G00387868@gmit.ie>
2020-04-10 20:54:44 +0100

    minor change to script

--- squareroot.py
+++ squareroot.py
@@ -1,9 +1,11 @@
 # John Kavanagh
 # need to create a function that finds the square root of any positive number
 
-x = (float(input("Type in any positive number: ")))
+x = (int(float(input("Type in any positive number: "))))
 
 def sqrt(x):
     return round(x ** (1/2), 1)
+
+# I'd like to prodce an error message if a positive integer isn't entered
 
 print("The square root of", x, "is approximately", sqrt(x))
