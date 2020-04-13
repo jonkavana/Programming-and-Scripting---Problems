0ce6ee0a9729abb73b0f491ac75fd6a8fc3c68d2
jonkavana <G00387868@gmit.ie>
2020-04-14 00:28:52 +0100

    coments in the plot.py file

--- Plot.py
+++ Plot.py
@@ -1,2 +1,4 @@
 # usign a function provided, read in the range the values that are in a range
-#reading material for this exercise.
+# reading material for this exercise: https://realpython.com/python-matplotlib-guide/
+# Trying to provide greater context at times: https://www.geeksforgeeks.org/python-introduction-matplotlib/
+# All material on the learnOnlein lessons as well: https://learnonline.gmit.ie/course/view.php?id=1598#section-9
